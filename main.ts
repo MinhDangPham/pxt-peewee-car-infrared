1d7a06f2db1aa7091bab6f4f7736571d580279ec
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-06-02 09:41:34 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
+type IRC = {
+    l: DigitalPin,
+    c: DigitalPin,
+    r: DigitalPin
+}
+const IR: IRC = {
+    l: DigitalPin.P14,
+    c: DigitalPin.P15,
+    r: DigitalPin.P13
+}
+pins.setPull(IR.l, PinPullMode.PullNone);
+pins.setPull(IR.c, PinPullMode.PullNone);
+pins.setPull(IR.r, PinPullMode.PullNone);
 
+//================================//
+
+let data: number;
+basic.forever(function () {
+    data = pins.digitalReadPin(IR.l);
+    console.log(data);
+    basic.pause(20)
+})
+
+//================================//
+
+// pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
+
+//================================//
+
+// pins.digitalReadPin(DigitalPin.P8)
